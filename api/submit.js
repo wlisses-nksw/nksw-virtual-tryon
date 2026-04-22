46560d4923d43e17e7083a24cae016934764baa5
wlisses-nksw <wlisses@nakedsw.com.br>
2026-04-22 15:54:02 -0300

    fix: usar model_name tryon-v1.6

--- api/submit.js
+++ api/submit.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        model_name: 'tryon',
+        model_name: 'tryon-v1.6',
         inputs: {
           model_image,
           garment_image,
